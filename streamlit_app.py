5a330228ced0d7ac5bcfec3c3c43684cadedfcb2
izzy-qtd <133872487+izzy-qtd@users.noreply.github.com>
2023-05-17 12:49:08 +0100

    Update streamlit_app.py
    Added emojis
    added second header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 streamlit.title('New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Oats and blueberry muffins')
-streamlit.text('Egg bites')
+streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
